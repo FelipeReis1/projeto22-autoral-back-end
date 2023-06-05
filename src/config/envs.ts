d1f57f0b6f89d818864b51be6d5fe831cf761b08
FelipeReis1 <feliperg15@yahoo.com.br>
2023-06-05 02:15:18 -0300

    feat: added jwt dotenv

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -12,3 +12,6 @@ export function loadEnv() {
   const currentEnvs = dotenv.config({ path });
   dotenvExpand.expand(currentEnvs);
 }
+export const jwtSecret =
+  process.env.JWT_SECRET ||
+  "0d29dfc99c684a43aeb11b7302c54f98e0dfdb63347f961dcf89b7f086e2ab45";
