e342200e202505d16dee0d66499e6a122e5f0f90
FelipeReis1 <feliperg15@yahoo.com.br>
2023-06-19 04:45:14 -0300

    feat: deleteProductStock and updateProductStock routes

--- src/routers/stocks-router.ts
+++ src/routers/stocks-router.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import {
   createProductStock,
   getProductStock,
+  deleteProductStock,
+  updateProductStock,
 } from "../controllers/stocks-controller";
 
 const stocksRouter = Router();
 
 stocksRouter.post("/:id", createProductStock);
 stocksRouter.get("/:id", getProductStock);
+stocksRouter.delete("/:id", deleteProductStock);
+stocksRouter.patch("/:id", updateProductStock);
 
 export { stocksRouter };
