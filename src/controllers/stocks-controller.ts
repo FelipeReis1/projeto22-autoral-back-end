dd842c0f30ab8bbe7ad4029eab3cbd57afb69ab1
FelipeReis1 <feliperg15@yahoo.com.br>
2023-06-19 04:44:15 -0300

    feat: deleteProductStock and updateProductStock

--- src/controllers/stocks-controller.ts
+++ src/controllers/stocks-controller.ts
@@ -27,3 +27,31 @@ export async function getProductStock(req: Request, res: Response) {
     return res.status(httpStatus.NOT_FOUND).send(error.message);
   }
 }
+
+export async function deleteProductStock(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    await stocksService.deleteProductStock(Number(id));
+    return res.sendStatus(httpStatus.NO_CONTENT);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.status(httpStatus.NOT_FOUND).send(error.message);
+    } else {
+      return res.status(httpStatus.BAD_REQUEST).send(error.message);
+    }
+  }
+}
+
+export async function updateProductStock(req: Request, res: Response) {
+  const { id } = req.params;
+  const { amount } = req.body;
+  try {
+    const productStock = await stocksService.updateProductStock(
+      Number(id),
+      amount
+    );
+    return res.status(httpStatus.OK).send(productStock);
+  } catch (error) {
+    return res.status(httpStatus.BAD_REQUEST).send(error.message);
+  }
+}
