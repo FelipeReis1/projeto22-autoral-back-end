8e1f90f1db6c4f5fa6c1750961b30973c388b06b
FelipeReis1 <feliperg15@yahoo.com.br>
2023-06-25 21:44:46 -0300

    feat: getCarts route

--- src/routers/cart-router.ts
+++ src/routers/cart-router.ts
@@ -1,16 +1,18 @@
 import { Router } from "express";
 import {
   getCart,
+  getCarts,
   createCart,
   deleteCart,
-  // updateCart,
+  //addToCart,
 } from "../controllers/cart-controller";
 
 const cartRouter = Router();
 
 cartRouter.get("/:id", getCart);
+cartRouter.get("/", getCarts);
 cartRouter.post("/", createCart);
 cartRouter.delete("/:id", deleteCart);
-// cartRouter.patch("/:id", updateCart);
+//cartRouter.patch("/:id", addToCart);
 
 export { cartRouter };
